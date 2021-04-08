82bee9600b1f52e271a8da4770bc48a3d76b7c1a
Michael Haraeb <haraebm@gmail.com>
2021-04-08 12:23:46 -0300

    Changed the colors of the painting to use the R,G,B color spectrum

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 
 from turtle import Turtle, Screen
-import turtle
+import turtle as t
 import random
-turtle.colormode(255)
+t.colormode(255)
 
-color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136),
-              (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149),
-              (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89),
-              (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208)]
+screen = Screen()
+screen.title("Hirst Painting")
+
+#R,G,B color creater, works together with colormode.
+def color_maker():
+    r = random.randint(1, 255)
+    g = random.randint(1, 255)
+    b = random.randint(1, 255)
+    color = (r, g, b)
+    return color
 
 
-
-color_choice = random.choice(color_list)
 pet = Turtle()
 pet.penup()
 pet.hideturtle()
@@ -21,13 +25,13 @@ pet.forward(300)
 pet.setheading(0)
 
 
-pet.pencolor(color_choice)
+#pet.pencolor(color_maker())
 
 game_on = 10
 
 while game_on > 0:
     for _ in range(10):
-        pet.dot(30, random.choice(color_list))
+        pet.dot(30,color_maker())
         pet.forward(50)
 
     pet.left(90)
@@ -38,5 +42,5 @@ while game_on > 0:
     game_on -= 1
 
 
-screen = Screen()
+
 screen.exitonclick()
